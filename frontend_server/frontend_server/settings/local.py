a98279c4828a52d7d631fec44f72f06da748d150
TannedCung <tando1903@gmail.com>
2024-07-16 03:49:20 +0700

    Add: logging

--- frontend_server/frontend_server/settings/local.py
+++ frontend_server/frontend_server/settings/local.py
@@ -131,6 +131,58 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_root")
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(module)s %(lineno)d - %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'file_daily': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': './django.log',
+            'when': 'D',  # Rotate daily
+            'interval': 1,
+            'backupCount': 7,  # Keep 7 backup logs
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file_daily', 'console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django.request': {
+            'handlers': ['file_daily', 'console'],
+            'level': 'INFO',
+            'propagate': True
+        },
+        'back_end': {  # Example for a custom logger
+            'handlers': ['file_daily', 'console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        '': {  # Example for a custom logger
+            'handlers': ['file_daily', 'console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        # Add more loggers as needed
+    }
+}
+
 
 
 
