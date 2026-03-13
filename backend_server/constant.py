9b28c018e9247ee3163fd450f58e5b379ae95191
TannedCung <tando1903@gmail.com>
2026-03-13 17:07:59 +0700

    feat: [US-020] - Refactor compress_sim_storage.py and remove all file storage infrastructure

--- backend_server/constant.py
+++ backend_server/constant.py
@@ -17,9 +17,6 @@ maze_assets_loc = "..//frontend_server/static_dirs/assets"
 env_matrix = f"{maze_assets_loc}/the_ville/matrix"
 env_visuals = f"{maze_assets_loc}/the_ville/visuals"
 
-fs_storage = "..//frontend_server/storage"
-# fs_storage = "frontend_server/storage"
-
 collision_block_id = "32125"
 
 # Verbose
